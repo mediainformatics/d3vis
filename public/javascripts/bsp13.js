a1afdaef94da2481ed0f5a8019701b6b9812ac4e
jurgen <jsinger@hs-harz.de>
2023-04-06 08:46:57 +0200

    fixed missing exports to window

--- public/javascripts/bsp13.js
+++ public/javascripts/bsp13.js
@@ -1,16 +1,16 @@
-const max =21, myData=[];
+const max = 21, myData = [];
 
 const colorScale = d3.scaleLinear()
     .domain([0, max])
     .range(["white", "#4169e1"]);
 
-const divergingScale = function(pivot) {
+const divergingScale = function (pivot) {
     return d3.scaleLinear()
         .domain([0, pivot, max])
         .range(["white", "#4169e1", "white"])
 };
 
-for(let i=0; i<max; ++i) myData.push(i);
+for (let i = 0; i < max; ++i) myData.push(i);
 
 function render(data, scale, selector) {
     const cells = d3.select(selector).selectAll("div.cell").data(data);
@@ -19,9 +19,14 @@ function render(data, scale, selector) {
         .append("div").merge(cells)
         .classed("cell", true)
         .style("display", "inline-block")
-        .style("background-color", d => scale(d) )
-        .text( (_d,i) => i);
+        .style("background-color", d => scale(d))
+        .text((_d, i) => i);
 }
 
 render(myData, colorScale, "#color");
 render(myData, divergingScale(5), "#color-diverge");
+
+window.render = render;
+window.colorScale = colorScale;
+window.divergingScale = divergingScale;
+window.myData = myData;
